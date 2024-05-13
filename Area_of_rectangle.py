72377b19ef905ef647feb359eb55e3ed4f3adb42
Haris Khan <hariskhan3428@gamil.com>
2024-05-13 17:07:19 +0500

    add try and except

--- Area_of_rectangle.py
+++ Area_of_rectangle.py
@@ -1,9 +1,12 @@
 # program to calculate the area of a rectangle
 print("----------------- Area of rectangle ---------------")
+try:
+    length = int(input("Enter length(m): "))
+    width = int(input("Enter width(m): "))
 
-length = int(input("Enter length(m): "))
-width = int(input("Enter width(m): "))
+    area_of_rectangle = length * width
 
-area_of_rectangle = length * width
+    print(f'\nArea of rectangle is {area_of_rectangle} m²')
 
-print(f'\nArea of rectangle is {area_of_rectangle} m²')
+except ValueError:
+    print("Invalid input! please enter an integer.")
