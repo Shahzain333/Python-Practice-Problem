8aa3c5a7f2f032521143c57d5f0cf23c530ed33a
Haris Khan <hariskhan3428@gamil.com>
2024-05-13 16:41:50 +0500

    area of rectangle

---
+++ Area_of_rectangle.py
@@ -0,0 +1,9 @@
+# program to calculate the area of a rectangle
+print("----------------- Area of rectangle ---------------")
+
+length = int(input("Enter length(m): "))
+width = int(input("Enter width(m): "))
+
+area_of_rectangle = length * width
+
+print(f'\nArea of rectangle is {area_of_rectangle} m²')
